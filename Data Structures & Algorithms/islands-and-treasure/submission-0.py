9034e24c398a815d9aea1c1680addd2f2fc69721
ShreyaK1601 <kaleshreya162001@gmail.com>
2026-04-01 16:09:49 -0700

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        q = deque()
+
+        def bfs(r, c):
+            if(min(r, c)<0 or r==rows or c==cols or (r, c) in visit or grid[r][c] == -1):
+                return
+            visit.add((r,c))
+            q.append([r, c])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 0:
+                    q.append([r, c])
+                    visit.add((r, c))
+        
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                grid[r][c] = dist
+                bfs(r+1, c)
+                bfs(r-1, c)
+                bfs(r, c+1)
+                bfs(r, c-1)
+            dist += 1
+        
